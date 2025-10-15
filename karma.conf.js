d677ea866d79cd10a1359e4cb3b9fb572e70202a
elisyanez <elis.yanez@duocuc.cl>
2025-10-15 14:33:48 -0300

    Se anadieron los archivos base para comenzar a colaborar

---
+++ karma.conf.js
@@ -0,0 +1,14 @@
+module.exports = function (config) {
+    config.set({
+        frameworks: ['jasmine'],
+        files: [
+            'src/utils/**/*.js', // incluye toda la lógica primero
+            '!src/utils/**/*.spec.js', // excluye los tests de este bloque
+            'src/**/*.spec.js' // incluye los tests después
+        ],
+        reporters: ['spec'], // reporter legible (recomendado)
+        browsers: ['ChromeHeadless'], // usa Chrome invisible
+        singleRun: true, // corre una vez y termina
+        concurrency: Infinity
+    });
+};
